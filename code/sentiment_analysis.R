5cf88f6f212f58fa240aef0e6f15a724c6ef8da2
zeinolf <zeinolf@yahoo.com>
2025-12-02 20:22:22 -0500

    Adding current version

---
+++ code/sentiment_analysis.R
@@ -0,0 +1,100 @@
+#i dont use all of these but for ease of running i've just copy/pasted
+library(rvest)
+library(tidyverse)
+library(dplyr)
+library(purrr)
+library(xml2)
+library(httr)
+library(httr2)
+library(acled.api)
+library(readr)
+library(stringr)
+library(huggingfaceR)
+library(DBI)
+library(RSQLite)
+library(scales)
+library(ggplot2)
+library(fastDummies)
+
+
+#setting up the huggingface model for sentiment analysis
+Sys.setenv(TOKENIZERS_PARALLELISM = "false")
+emotion_distilroBERTa <- hf_load_pipeline(
+  model_id = "j-hartmann/emotion-english-distilroberta-base",
+  task     = "text-classification"
+)
+
+#setting up a function to run the sentiment analysis
+score_emotions_one <- function(txt) {
+  if (is.na(txt) || !nzchar(txt)) {
+    return(tibble(label = NA_character_, score = NA_real_))
+  }
+  txt_use <- substr(txt, 1, 1000)
+  out <- emotion_distilroBERTa(txt_use, return_all_scores = TRUE)
+  
+  
+  if (is.list(out) && length(out) > 0 && !is.null(out[[1]]$label)) {
+    out_list <- out
+  } else if (is.list(out) && length(out) > 0 && is.list(out[[1]]) && !is.null(out[[1]][[1]]$label)) {
+    out_list <- out[[1]]
+  } else {
+    return(tibble(label = NA_character_, score = NA_real_))
+  }
+  
+  labels <- sapply(out_list, `[[`, "label")
+  scores <- sapply(out_list, `[[`, "score")
+  
+  tibble(
+    label = labels,
+    score = scores
+  )
+}
+
+#reading in nigeria news data
+con <- dbConnect(SQLite(), "./data/nigeria_news.db")
+article_data <- dbReadTable(con, "nigeria_news")
+dbDisconnect(con)
+
+#adding doc_id
+article_data_id <- article_data |>
+  mutate(doc_id = row_number())
+
+#running sentiment analysis
+emotion_long <- map2_dfr(
+  article_data_id$text,
+  article_data_id$doc_id,
+  ~ {df <- score_emotions_one(.x)
+  df$doc_id <- .y
+  df
+  }
+) |>
+  relocate(doc_id, label, score)|>
+  left_join(
+    article_data_id |> 
+      select(doc_id, pub_date_raw, source, title), by = "doc_id"
+  )
+
+#cleaning and formatting
+emotion_time <- emotion_long |>
+  mutate(
+    date  = suppressWarnings(mdy(pub_date_raw)),
+    month = floor_date(date, unit = "month")
+  ) |>
+  filter(!is.na(month), !is.na(label)) |>
+  group_by(month, label) |>
+  summarize(
+    mean_score = mean(score, na.rm = TRUE),
+    n_articles = n_distinct(doc_id),
+    .groups    = "drop"
+  )
+
+
+#writing to a new db
+con <- dbConnect(SQLite(), "./data/emotion_time.db")
+dbWriteTable(con, "emotion_time", emotion_time, overwrite = TRUE)
+dbDisconnect(con)
+
+
+con <- dbConnect(SQLite(), "./data/emotion_long.db")
+dbWriteTable(con, "emotion_long", emotion_long, overwrite = TRUE)
+dbDisconnect(con)
